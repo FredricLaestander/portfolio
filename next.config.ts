5a1b1a7eef93a7eb627a145673be3d2b305d8a9e
Fredric Laestander <fredriclaestander@gmail.com>
2025-07-31 19:47:30 +0200

    allow sanity images

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      new URL("https://cdn.sanity.io/images/7xghh627/production/**"),
+    ],
+  },
   /* config options here */
 };
 
