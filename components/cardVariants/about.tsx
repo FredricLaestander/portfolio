c69dfd48ceb4222551a1500a5fff00521dc83513
Fredric Laestander <fredriclaestander@gmail.com>
2025-08-04 00:39:21 +0200

    create about component

---
+++ components/cardVariants/about.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import { PortableText } from 'next-sanity'
+import { Card } from '../card'
+import { Modal } from '../modal'
+import Image from 'next/image'
+import { urlFor } from '@/sanity/lib/image'
+import { About as TAbout } from '@/sanity/types'
+import { useState } from 'react'
+
+export const About = ({ about }: { about: TAbout }) => {
+  const [openModal, setOpenModal] = useState(false)
+
+  return (
+    <>
+      <Card title='about' onClick={() => setOpenModal(true)}>
+        {about.images.map((image) => {
+          const url = urlFor(image).url()
+          return <Image key={url} src={url} alt='' width={45} height={80} />
+        })}
+      </Card>
+
+      {openModal && (
+        <Modal close={() => setOpenModal(false)}>
+          <h3 className='w-full text-lg'>About</h3>
+
+          <PortableText value={about.introduction} />
+          <div className='flex w-full gap-2 overflow-hidden rounded-sm'>
+            {about.images.map((image) => {
+              const url = urlFor(image).url()
+              return (
+                <Image
+                  key={url}
+                  src={url}
+                  alt=''
+                  width={160}
+                  height={256}
+                  className='object-cover'
+                />
+              )
+            })}
+          </div>
+          <PortableText value={about.description} />
+        </Modal>
+      )}
+    </>
+  )
+}
