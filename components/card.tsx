4a00af2c032d50c93b956eedbfa1ba4e2790f5c0
Fredric Laestander <fredriclaestander@gmail.com>
2025-08-03 23:04:49 +0200

    create card and modal components

--- components/card.tsx
+++ components/card.tsx
@@ -1,33 +1,23 @@
 'use client'
 
-import Image from 'next/image'
-import { Modal } from './modal'
-import { useState } from 'react'
-import { About } from '@/sanity/types'
-import { urlFor } from '@/sanity/lib/image'
+import { ReactNode } from 'react'
 
-export const Card = ({ title, about }: { title: string; about: About }) => {
-  const [openModal, setOpenModal] = useState(false)
-
+export const Card = ({
+  children,
+  title,
+  onClick,
+}: {
+  children: ReactNode
+  title: string
+  onClick: () => void
+}) => {
   return (
-    <>
-      <button
-        onClick={() => {
-          setOpenModal(true)
-        }}
-        className='flex h-60 w-48 flex-col gap-4 rounded-lg border-2 border-slate-200 bg-slate-100 p-8'
-      >
-        <div className='bg-skyblue size-32 rounded-full'>
-          {about.images.map((image) => {
-            const url = urlFor(image).url()
-            return <Image key={url} src={url} alt='' width={45} height={80} />
-          })}
-        </div>
-
-        <p className='text-lg font-bold text-slate-700'>{title}</p>
-      </button>
-
-      {openModal && <Modal about={about} />}
-    </>
+    <button
+      onClick={onClick}
+      className='flex h-60 w-48 flex-col gap-4 rounded-lg border-2 border-slate-200 bg-slate-100 p-8'
+    >
+      <div className='bg-skyblue size-32 rounded-full'>{children}</div>
+      <p className='text-lg font-bold text-slate-700'>{title}</p>
+    </button>
   )
 }
--- components/modal.tsx
+++ components/modal.tsx
@@ -1,11 +1,22 @@
-import { About } from '@/sanity/types'
-import { PortableText, PortableTextBlock } from '@portabletext/react'
+import { ReactNode } from 'react'
 
-export const Modal = ({ about }: { about: About }) => {
+export const Modal = ({
+  children,
+  close,
+}: {
+  children: ReactNode
+  close: () => void
+}) => {
   return (
-    <div className='flex w-3xl flex-col gap-4 rounded-3xl px-20 pt-8'>
-      <h3 className='w-full bg-slate-800 text-lg'>About</h3>
-      <PortableText value={about.introduction} />
-    </div>
+    <>
+      <div
+        onClick={close}
+        className='fixed inset-0 bg-white/20 backdrop-blur-lg'
+      ></div>
+
+      <div className='absolute top-1/2 left-1/2 z-10 flex w-3xl -translate-x-1/2 -translate-y-1/2 flex-col gap-4 rounded-3xl bg-white px-20 py-8'>
+        {children}
+      </div>
+    </>
   )
 }
