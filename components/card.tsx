97d9e04523a7b5fd08e44b58ae1286cff5639b22
Fredric Laestander <fredriclaestander@gmail.com>
2025-08-03 23:50:19 +0200

    center children and add cursor-pointer on cards

--- components/card.tsx
+++ components/card.tsx
@@ -14,9 +14,11 @@ export const Card = ({
   return (
     <button
       onClick={onClick}
-      className='flex h-60 w-48 flex-col gap-4 rounded-lg border-2 border-slate-200 bg-slate-100 p-8'
+      className='flex h-60 w-48 cursor-pointer flex-col gap-4 rounded-lg border-2 border-slate-200 bg-slate-100 p-8'
     >
-      <div className='bg-skyblue size-32 rounded-full'>{children}</div>
+      <div className='bg-skyblue flex size-32 items-center justify-center rounded-full'>
+        {children}
+      </div>
       <p className='text-lg font-bold text-slate-700'>{title}</p>
     </button>
   )
