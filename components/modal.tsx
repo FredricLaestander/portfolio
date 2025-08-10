51f5f8b60c22d0dfc251ed76eec4592a99b85bb4
Fredric Laestander <fredriclaestander@gmail.com>
2025-08-10 19:27:17 +0200

    fix modal

--- components/modal.tsx
+++ components/modal.tsx
@@ -12,7 +12,7 @@ export const Modal = ({
   return (
     <div
       onClick={close}
-      className={`fixed inset-0 z-10 flex flex-col items-center gap-4 overflow-y-scroll rounded-3xl bg-slate-400/20 pt-20 pb-8 backdrop-blur-lg ${centered && 'justify-center'}`}
+      className={`fixed inset-0 z-20 flex flex-col items-center gap-4 overflow-y-scroll rounded-3xl bg-slate-400/20 pt-20 pb-8 backdrop-blur-lg ${centered && 'justify-center'}`}
     >
       {children}
     </div>
