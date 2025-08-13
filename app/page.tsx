e4ea8db5702eb74c82d1f53602e42401a0c304bb
Fredric Laestander <fredriclaestander@gmail.com>
2025-08-13 20:11:13 +0200

    fix social logos

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import happy from '../public/happy.svg'
-import { Github, Linkedin, Mail } from 'lucide-react'
+import linkedIn from '../public/linkedIn.svg'
+import { Mail } from 'lucide-react'
 import { getAbout, getProjects } from '@/sanity/routes'
 import { About } from '@/components/cardVariants/about'
 import { Skills } from '@/components/cardVariants/skills'
 import { Projects } from '@/components/cardVariants/projects'
+import { SiGithub } from '@icons-pack/react-simple-icons'
 
 export default async function Home() {
   // const heroText = await getHeroText();
@@ -35,15 +37,15 @@ export default async function Home() {
           href='https://github.com/FredricLaestander/'
           className='flex items-center gap-1 text-sm font-bold'
         >
-          <Github className='size-6' />
-          <p className='text-sm'>Github</p>
+          <SiGithub className='size-6' />
+          Github
         </a>
 
         <a
           href='https://www.linkedin.com/in/fredric-laestander/'
           className='flex items-center gap-1 text-sm font-bold'
         >
-          <Linkedin className='size-6' />
+          <Image src={linkedIn} alt='linkedin' className='size-6' />
           LinkedIn
         </a>
 
