e75e120789b27a4b17bee1a3f44a63cbb05666b5
Fredric Laestander <fredriclaestander@gmail.com>
2025-08-14 15:26:48 +0200

    create social component

---
+++ components/social.tsx
@@ -0,0 +1,18 @@
+import { ComponentType, ReactElement, ReactNode } from 'react'
+
+export const Social = ({
+  href,
+  icon: Icon,
+  children,
+}: {
+  href: string
+  icon: ComponentType<{ className?: string }>
+  children: ReactNode
+}) => {
+  return (
+    <a href={href} className='flex items-center gap-1 text-sm font-bold'>
+      <Icon className='size-7' />
+      {children}
+    </a>
+  )
+}
