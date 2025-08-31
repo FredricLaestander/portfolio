950629064bb1fa4fcb5b5fbf6b9e7f91b329bf78
Fredric Laestander <fredriclaestander@gmail.com>
2025-08-31 19:34:05 +0200

    create toggle darkmode switch

--- components/themeSwitch.tsx
+++ components/themeSwitch.tsx
@@ -15,7 +15,7 @@ export const ThemeSwitch = () => {
       onClick={toggleTheme}
       className='flex h-6 w-12 cursor-pointer items-center rounded-full border-2 border-slate-200 bg-slate-100 p-0.5 transition dark:border-slate-800 dark:bg-slate-700'
     >
-      <div className='flex size-5 items-center justify-center rounded-full bg-slate-200 transition-transform dark:translate-x-[100%] dark:bg-slate-800'>
+      <div className='theme-toggle-transition flex size-5 items-center justify-center rounded-full bg-slate-200 dark:translate-x-[100%] dark:bg-slate-800'>
         <Sun className='size-4 rounded-full text-slate-700 dark:hidden dark:text-slate-400' />
         <Moon className='hidden size-4 rounded-full text-slate-400 dark:block dark:text-slate-200' />
       </div>
